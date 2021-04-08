73bf182c48bed3cb6d4591ccbc98b14b0c912478
knospfer <berrueta.mattia@gmail.com>
2021-04-08 08:02:06 +0200

    feat: show input messages

---
+++ src/commands/new-file.ts
@@ -0,0 +1,52 @@
+import * as mkdirp from "mkdirp";
+import { lstatSync } from "node:fs";
+import { InputBoxOptions, OpenDialogOptions, Uri, window } from "vscode";
+
+export const newFile = async (uri: Uri) => {
+    const fileName = await promptForFileName();
+    if (!fileName) {
+        window.showErrorMessage("Error: file name is missing");
+        return;
+    }
+
+    let directory;
+    const hasDirectory = uri?.fsPath || lstatSync(uri?.path).isDirectory();
+    if (!hasDirectory) {
+        directory = await promptForTargetDirectory();
+        if (!directory) {
+            window.showErrorMessage("Error: select a valid directory");
+            return;
+        }
+    } else {
+        directory = uri.fsPath;
+    }
+
+    //TODO GENERATE FILE
+};
+
+//returns a promise
+function promptForFileName(): Thenable<string | undefined> {
+    const alertOptions: InputBoxOptions = {
+        prompt: "File Name",
+        placeHolder: "my-file"
+    };
+    return window.showInputBox(alertOptions);
+}
+
+async function promptForTargetDirectory() {
+    const options: OpenDialogOptions = {
+        canSelectMany: false,
+        canSelectFolders: true,
+        openLabel: "Select a folder to place file"
+    };
+
+    const uri = await window.showOpenDialog(options);
+
+    if (!!uri) {
+        return uri[0].path;
+    }
+    return null;
+}
+
+//TODO GENERATE CODE
+
