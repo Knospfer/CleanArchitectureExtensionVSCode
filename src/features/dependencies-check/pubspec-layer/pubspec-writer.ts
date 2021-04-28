d1559deda558573e53b1c91738ff0ade01bc1e3c
knospfer <berrueta.mattia@gmail.com>
2021-04-28 08:16:37 +0200

    feat: writer variadic function

--- src/features/dependencies-check/pubspec-layer/pubspec-writer.ts
+++ src/features/dependencies-check/pubspec-layer/pubspec-writer.ts
@@ -4,16 +4,16 @@ import { PubspecReader } from "./pubspec-reader";
 import { PubpsecPathGetter } from "./pusbpec-getter";
 
 export interface PubspecWriter {
-    writePubspec(): Promise<void>
+    writePubspec(...depencencies: string[]): Promise<void>
 }
 
 export class PubspecWriterConcrete implements PubspecWriter {
     constructor(private pubGetter: PubpsecPathGetter, private pubReader: PubspecReader) { }
 
-    async writePubspec() {
+    async writePubspec(...depencencies: string[]) {
         if (workspace.workspaceFolders && workspace.workspaceFolders.length > 0) {
             const path = this.pubGetter.getPubspecPath();
-            const arrayBuffer = await this.insertRowBeforeDevDependencies();
+            const arrayBuffer = await this.insertRowBeforeDevDependencies(...depencencies);
 
             await workspace.fs.writeFile(
                 Uri.file(path),
@@ -23,13 +23,18 @@ export class PubspecWriterConcrete implements PubspecWriter {
         throw Error("NO environment!!");
     }
 
-    private async insertRowBeforeDevDependencies(): Promise<Uint8Array> {
+    private async insertRowBeforeDevDependencies(...depencencies: string[]): Promise<Uint8Array> {
         let decodedString = await this.pubReader.readPubspec();
+
+        const concatDependencies = depencencies.reduce((prev, current) => {
+            if (!!prev) { return current; }
+            return `${prev}\n  ${current}\n`;
+        }, "");
 
         decodedString = decodedString.split("dev_dependencies:")
             .reduce((prev, current) => {
                 if (!!prev) {
-                    return `${prev}  #auto generated dependencies\n  flutter_bloc:\n  injectable:\n  dartz:\ndev_dependencies:${current}`;
+                    return `${prev}  #auto generated dependencies\n  ${concatDependencies}dev_dependencies:${current}`;
                 }
                 return current;
             }, "");
